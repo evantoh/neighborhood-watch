5192bcec4ddfa09517464c78635fb64b03cd9016
evantoh <evanmwenda@gmail.com>
2018-03-16 17:50:44 +0300

    created form that will help in creation ,deletion and updating the user

---
+++ neighborupdate/forms.py
@@ -0,0 +1,9 @@
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = ProfileUser
+        fields = ('url', 'neighborhood_location', 'company')
--- neighborupdate/views.py
+++ neighborupdate/views.py
@@ -1,5 +1,9 @@
-from django.http  import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('testing!!!')
+    return render(request, 'all-temps/welcome.html')
+def update_profile(request,user_id):
+    user=User.objects.get(pk=user_id)
+    user.profile.bio='ffgfdvnbdfgcbfghnhjykjm'
+    user.save()
