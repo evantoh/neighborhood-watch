2c1e2a2340bf013cbb8b39ca466166a706c05d2d
evantoh <evanmwenda@gmail.com>
2018-03-16 18:02:53 +0300

    created a view to handle updating profile

--- neighborupdate/views.py
+++ neighborupdate/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.db import transaction
+
 
 # Create your views here.
 def welcome(request):
+ 
     return render(request, 'all-temps/welcome.html')
-def update_profile(request,user_id):
-    user=User.objects.get(pk=user_id)
-    user.profile.bio='ffgfdvnbdfgcbfghnhjykjm'
-    user.save()
+@login_required
+@transaction.atomic
+def update_profile(request):
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(request.POST, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, _('Your profile was successfully updated!'))
+            return redirect('settings:profile')
+        else:
+            messages.error(request, _('Please correct the error below.'))
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = ProfileForm(instance=request.user.profile)
+    return render(request, 'profiles/profile.html', {
+        'user_form': user_form,
+        'profile_form': profile_form
+    })
