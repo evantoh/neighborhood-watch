be8df6ce05498131c2821bb6f46176d1b1651110
evantoh <evanmwenda@gmail.com>
2018-03-16 16:20:30 +0300

    setup registration fully

--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap3',
+    'neighborupdate',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- neighborupdate/models.py
+++ neighborupdate/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 # create model neighborhood
-class Neighbourhood(models.Model):
-    neighborhood_name = models.CharField(max_length =30)
-    neighborhood_location = models.CharField(max_length =30)
-    occupants_count = models.IntegerField(default=0)
-    admin=models.ForeignKey(User,null=True)
+# class Neighbourhood(models.Model):
+#     neighborhood_name = models.CharField(max_length =30)
+#     neighborhood_location = models.CharField(max_length =30)
+#     occupants_count = models.IntegerField(default=0)
+#     admin=models.ForeignKey(User,null=True)
 
     
