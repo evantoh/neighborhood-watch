c8145b432be0d7b7ca5721220f5071de7bbcf0d6
evantoh <evanmwenda@gmail.com>
2018-03-17 20:34:17 +0300

    registered the models to admin

--- neighborupdate/admin.py
+++ neighborupdate/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile,Neighbourhood,Business
 
 # Register your models here.
 admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Neighbourhood)
