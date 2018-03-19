3f43457bd2f640b955d3124141ec31a9ff34032d
evantoh <evanmwenda@gmail.com>
2018-03-19 16:19:20 +0300

    about to deploy

--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -83,7 +86,7 @@ DATABASES = {
     'PASSWORD':'evans123',
     }
 }
-
+DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
